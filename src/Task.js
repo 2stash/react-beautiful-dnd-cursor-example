71d2e25c0dacb16c47c5083196695a97dcfbcaeb
Daniel Westlund <dwestlund531@gmail.com>
2020-11-23 18:12:10 -0600

    initial commit

---
+++ src/Task.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Draggable } from "react-beautiful-dnd";
+
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  background-color: #fff;
+  cursor: ${props => (props.isDragDisabled ? 'not-allowed' : 'grab')}
+`;
+
+export default class Task extends Component {
+  render() {
+    const isDragDisabled = this.props.task.id === 'task-1';
+    return (
+      <Draggable draggableId={this.props.task.id} index={this.props.index} 
+      // isDragDisabled here disables dragging
+      isDragDisabled={isDragDisabled}>
+        {(provided) => (
+          <Container {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} 
+          // isDragDisabled here added to provided styling
+          isDragDisabled={isDragDisabled}>
+            {this.props.task.content}
+          </Container>
+        )}
+      </Draggable>
+    );
+  }
+}
